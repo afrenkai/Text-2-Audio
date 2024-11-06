14d197bf84df56195d9842530fc0626127b6c096
Sid Pradhan <64797131+SiddharthaPradhan@users.noreply.github.com>
2024-11-06 03:27:44 -0500

    updated req.txt

--- get_data.py
+++ get_data.py
@@ -1,8 +1,12 @@
 from datasets import load_dataset
+import os
+
+
+def save_lj_speech_dataset(save_dir='./data/'):
+    os.makedirs(save_dir, exist_ok = True) # if dir not made make it else nothing
+    hf_dataset = load_dataset('keithito/lj_speech')
+    hf_dataset.save_to_disk(save_dir)
+    print('Saved LJ Speech Dataset to disk')
 
 if __name__ == "__main__":
-    SAVE_DIR = './data/'
-    import os
-    os.makedirs(SAVE_DIR, exist_ok = True) #if dir not made make it else nothing
-    hf_dataset = load_dataset('keithito/lj_speech')
-    hf_dataset.save_to_disk(SAVE_DIR)
+    save_lj_speech_dataset()
