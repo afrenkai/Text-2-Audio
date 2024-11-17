98946daf2ff72257226a2e74f08af2ae27b73afd
Sid Pradhan <64797131+SiddharthaPradhan@users.noreply.github.com>
2024-11-17 01:45:04 -0500

    updated slurm script

--- test_main.sh
+++ test_main.sh
@@ -2,11 +2,11 @@
 #SBATCH -N 1
 #SBATCH -n 8
 #SBATCH --mem=50g
-#SBATCH -J "TTS Simple Test"
+#SBATCH -J "TTS seq2seq tacomini"
 #SBATCH -p short
-#SBATCH -t 16:00:00
+#SBATCH -t 24:00:00
 #SBATCH --gres=gpu:1
-#SBATCH --output=/home/sppradhan/Text-2-Audio/tts_simple_%j.txt
+#SBATCH --output=/home/sppradhan/Text-2-Audio/seq2seq_%j.txt
 
 module load cuda
 module load python/3.10.13
